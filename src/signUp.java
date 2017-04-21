a4cd40277a20e7a3f5ed9bc8892238e4ca8759cf
Devershi Chandra <devershichandra27@gmail.com>
2017-04-21 15:23:04 +0530

    Added required signUp files

--- src/signUp.java
+++ src/signUp.java
@@ -6,37 +6,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpSession;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
+import com.VerificationServlet;
 
 public class signUp extends HttpServlet
 {
-	public static final String username ="root";
-	public static final String password = "Gmail@123";
-	
-	public static boolean validate(String username, String password)
-	{
-		boolean status = false;
-		try
-		{
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/assignment", username, password);
-
-			String sql = "Select * from user_table where username = ? and password = ?";
-			PreparedStatement ps = con.prepareStatement(sql);
-
-			ps.setString(1, username);
-			ps.setString(2, password);
-
-			ResultSet rs = ps.executeQuery();
-			status = rs.next();
-		}
-		catch(Exception e){System.out.println(e);}
-		return status;
-	}
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
 		response.setContentType("text/html");
@@ -44,15 +19,16 @@ public class signUp extends HttpServlet
 
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
+		// String sql = "Select * from user_table where username = ? and password = ?";
 
-		if(validate(username, password))
-		{
+		if(VerificationServlet.validate(username, password))
+		{	
 			RequestDispatcher requestDispatcher = request.getRequestDispatcher("/welcome");
 			requestDispatcher.forward(request, response);
 		}
 		else
 		{
-			out.println("Sorry incorrect username or password");
+			//out.println("Sorry incorrect username or password " + username + " " + password);
 			RequestDispatcher requestDispatcher = request.getRequestDispatcher("signUp.html");
 			requestDispatcher.include(request, response);
 		}
