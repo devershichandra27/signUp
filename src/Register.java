399e0d238ef859a769649ae518e8cd5ac66ec59f
Devershi Chandra <devershichandra27@gmail.com>
2017-04-26 03:24:25 +0530

    Automatic login after account creation

--- src/Register.java
+++ src/Register.java
@@ -1,11 +1,12 @@
 import javax.servlet.http.*;
 import java.io.*;
+import javax.servlet.*;
 
 import com.VerificationServlet;
 
 public class Register extends HttpServlet
 {
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,  IOException
 	{
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
@@ -18,13 +19,19 @@ public class Register extends HttpServlet
 		if(VerificationServlet.checkExistingUser(username))
 		{
 			out.println("Username already exists");
+			request.getRequestDispatcher("/register").include(request, response);
 		}
 		else
 		{
 			String firstName = request.getParameter("firstName");
 			String lastName = request.getParameter("lastName");
 			VerificationServlet.insertNewUser(firstName, lastName, username, password);
+			HttpSession session = request.getSession();
+			session.setAttribute("username", username);
+			session.setAttribute("firstName", firstName);
+			session.setAttribute("lastName", lastName);
 			out.println("Account Created.");
+			request.getRequestDispatcher("/home").include(request, response);
 		}
 
 	}
